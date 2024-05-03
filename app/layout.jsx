1c2e8855a7d25240ff68dbe6b541c9343dc70b21
cceph <96626515+CCeph@users.noreply.github.com>
2024-05-03 12:02:34 -0300

    Design nav bar

---
+++ app/layout.jsx
@@ -0,0 +1,33 @@
+import { Inter } from "next/font/google";
+import Image from "next/image";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "D&D Companion Tool",
+  description: "A tool to help geenrate NPCs, quests, and stories for DND",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <nav className="bg-black flex items-center justify-center gap-10 p-5">
+          <div className="flex flex-col items-center">
+            <h1 className="font-dndTitle text-4xl text-red-600">Dungeons</h1>
+            <h1 className="font-dndTitle text-4xl text-red-600">&</h1>
+            <h1 className="font-dndTitle text-4xl text-red-600">Dragons</h1>
+          </div>
+          <Image 
+            src="/appLogoMini.png"
+            alt="DND Companion App logo"
+            width={125}
+            height={125}
+          />
+          <h1 className="font-dndTitle text-4xl text-white">Companion</h1>
+        </nav>
+        {children}</body>
+    </html>
+  );
+}

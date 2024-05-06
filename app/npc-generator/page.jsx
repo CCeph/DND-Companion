6d322a0bb90bc1455de6f177bcf16fd4f043ec9c
cceph <96626515+CCeph@users.noreply.github.com>
2024-05-06 11:06:57 -0300

    Create initial draft of NPC generator form

--- app/npc-generator/page.jsx
+++ app/npc-generator/page.jsx
@@ -1,3 +1,61 @@
 export default function NPCForm() {
-  return <div>Test</div>;
+  async function generateNPC(formData) {
+    "use server";
+
+    const rawFormData = {
+      race: formData.get("race"),
+      occupation: formData.get("occupation"),
+      class: formData.get("class"),
+      otherDetails: formData.get("otherDetails"),
+    };
+
+    console.log(rawFormData);
+  }
+
+  return (
+    <main className="flex flex-1 justify-center">
+      <form action={generateNPC} className="m-5 flex max-w-2xl flex-1 flex-col">
+        <h1 className="place-self-center text-4xl font-semibold">
+          NPC Generator
+        </h1>
+        <div className="m-5 flex flex-1 flex-col">
+          <label htmlFor="race">
+            NPC Race <span className="text-red-600">*</span>
+          </label>
+          <input
+            type="text"
+            id="race"
+            name="race"
+            className="my-2 rounded-sm border-2 p-2"
+          />
+          <label htmlFor="occupation">Occupation:</label>
+          <input
+            type="text"
+            name="occupation"
+            id="occupation"
+            className="my-2 rounded-sm border-2 p-2"
+          />
+          <label htmlFor="class">Class:</label>
+          <input
+            type="text"
+            name="class"
+            id="class"
+            className="my-2 rounded-sm border-2 p-2"
+          />
+          <label htmlFor="otherDetails">Other Details:</label>
+          <textarea
+            name="otherDetails"
+            id="otherDetails"
+            className="my-2 rounded-sm border-2 p-2"
+          ></textarea>
+          <button
+            type="submit"
+            className="my-2 place-self-center rounded-xl border-2 bg-black p-3 text-lg text-white"
+          >
+            Generate
+          </button>
+        </div>
+      </form>
+    </main>
+  );
 }

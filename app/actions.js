29b3386031f454bf8079afdbe58d9886623dd6d8
cceph <96626515+CCeph@users.noreply.github.com>
2024-05-07 01:40:52 -0300

    Move server action to actions.js

---
+++ app/actions.js
@@ -0,0 +1,64 @@
+"use server";
+
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+function generateUserPrompt(rawFormData) {
+  let userPrompt = "";
+  if (!(rawFormData.race === "")) {
+    userPrompt += `The NPC's race is: ${rawFormData.race}\n`;
+  }
+  if (!(rawFormData.gender === "")) {
+    userPrompt += `Their gender is: ${rawFormData.gender}\n`;
+  }
+  if (!(rawFormData.occupation === "")) {
+    userPrompt += `Their occupation is: ${rawFormData.occupation}\n`;
+  }
+  if (!(rawFormData.class === "")) {
+    userPrompt += `Their DND class is: ${rawFormData.class}\n`;
+  }
+  if (!(rawFormData.otherDetails === "")) {
+    userPrompt += `Other details about them: ${rawFormData.otherDetails}\n`;
+  }
+  if (userPrompt === "") {
+    userPrompt = "The user did not provide any additional information.";
+  }
+  return userPrompt;
+}
+
+async function generateNPC(formData) {
+  "use server";
+
+  const rawFormData = {
+    race: formData.get("race"),
+    gender: formData.get("gender"),
+    occupation: formData.get("occupation"),
+    class: formData.get("class"),
+    otherDetails: formData.get("otherDetails"),
+  };
+
+  console.log(rawFormData);
+  const userPrompt = generateUserPrompt(rawFormData);
+
+  console.log(userPrompt);
+
+  /* const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a DND Dungeon Master. You will be given some information by the user about a NPC You need to generate a name (max 50 words), background (max 150 words), and personality (max 100 words) for the NPC.",
+        },
+        {
+          role: "user",
+          content: userPrompt,
+        },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+    console.log(completion);
+    console.log(completion.choices[0]);
+    console.log(completion.choices[0].message.content); */
+  return "User Prompt goes here";
+}
